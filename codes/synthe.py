12f85c921fdc1f405f215ccbaf92e61663bafb68
Alban Benmouffek <49032546+sonibla@users.noreply.github.com>
2019-12-15 15:12:12 +0000

    Update synthe.py
    Added comments

--- codes/synthe.py
+++ codes/synthe.py
@@ -2,15 +2,27 @@
 import pygame
 import serial
 import time
-ser = serial.Serial('/dev/ttyACM0',9600)
+
+# This code will play piano sounds according to recieved serial data
+
+# /dev/ttyACM0 corresponds to Arduino USB connection
+# 9600 baud is enough
+ser = serial.Serial('/dev/ttyACM0', 9600)
+debug = False
+
+# You may change the following settings to customize your piano
+# Refer to : https://www.pygame.org/docs/ref/mixer.html#pygame.mixer.pre_init
 pygame.mixer.pre_init(22050,-16,2,64)
 pygame.mixer.init()
 pygame.init()
 
 while True :
-    s=str(int( ser.readline(),16 ))
-    print(s)
-    chemin = '/home/alban/Documents/WAV/' + s + ".wav"
-    pygame.mixer.music.load(chemin)
-    pygame.mixer.music.play(0)
+    number = str(int(ser.readline(), 16))
+    # number corresponds to one piano key (from 1 to 88)
     
+    if debug:
+        print(number)
+    
+    file = '../WAV/' + number + ".wav"
+    pygame.mixer.music.load(file)
+    pygame.mixer.music.play(0)
